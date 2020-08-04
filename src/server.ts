8567aab622a0f94b31e194fb5ab1c5a972590593
josenogueira_7egend <jose.nogueira@7egend.cr>
2020-08-04 23:46:23 +0100

    add jwt

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,28 @@ import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { UserResolver } from './resolvers/User'
+import cookieParser from 'cookie-parser'
 
 (async () => {
-  const app = express()
-
-  await createConnection()
-
-  const apolloServer = new ApolloServer({
+  const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [UserResolver]
     }),
     context: ({ req, res }) => ({ req, res })
   })
 
-  apolloServer.applyMiddleware({ app, cors: false })
+  await createConnection()
+
+  const app = express()
+
+  app.use(cookieParser())
+
+  app.use((req, _, next) => {
+    console.log(req.cookies)
+    next()
+  })
+
+  server.applyMiddleware({ app, cors: false })
 
   app.listen(4000, () => {
     console.log('express server started')
